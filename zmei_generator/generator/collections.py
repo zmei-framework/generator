d2381fd19e22b215eae95e4f9bf3c4895cc2b360
Aleksandr Rudakov <alex@negative.ee>
2019-01-14 01:33:29 +0200

    Small bugfix for gitlab tests

--- zmei_generator/generator/collections.py
+++ zmei_generator/generator/collections.py
@@ -275,16 +275,24 @@ def generate_common_files(target_path, skeleton_dir, apps):
         if collection_set.gitlab:
             for file in [
                 '.gitlab-ci.yml',
-                'README.md',
-                'conftest.py',
-                'pytest.ini',
-                'requirements.dev.txt',
-                '.flake8',
-                '.coveragerc'
+                'README.md'
             ]:
                 generate_file(target_path, file, f"gitlab/{file.strip('.')}.tpl", {
                     'gitlab': collection_set.gitlab,
                 })
+            if collection_set.gitlab.test:
+                for file in [
+                    'conftest.py',
+                    'pytest.ini',
+                    'requirements.dev.txt',
+                    '.flake8',
+                    '.coveragerc'
+                ]:
+                    generate_file(target_path, file, f"gitlab/{file.strip('.')}.tpl", {
+                        'gitlab': collection_set.gitlab,
+                    })
+
+                generate_file(target_path, 'app/settings_test.py', template_name='settings.py.tpl')
 
 
 def generate(target_path, app_name: str, collection_set: CollectionSetDef, features=None):
