89601a3786a49fcfccf25ec40e6f38239bdef88f
Aleksandr Rudakov <alex@negative.ee>
2019-01-26 14:15:08 +0200

    Removed some old files

--- docker_app/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker build -t zmeigen/app . && docker push zmeigen/app && ssh root@genius-apps.com docker pull zmeigen/app
--- github.py
+++
@@ -1,3 +0,0 @@
-import requests
-r = requests.get('https://api.github.com/repos/')
-
